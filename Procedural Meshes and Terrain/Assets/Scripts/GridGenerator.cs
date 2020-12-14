102528f4a61fcb20ae52e05d3d989fc33230d977
Jack Gindi <38888998+jmgindi@users.noreply.github.com>
2020-12-14 14:01:30 -0800

    add terrain generator, initial commit

---
+++ Procedural Meshes and Terrain/Assets/Scripts/GridGenerator.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class GridGenerator : MonoBehaviour
+{
+    public int xSize, zSize;
+    private Vector3[] vertices;
+    private Mesh mesh;
+
+    private void Generate() {
+        GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+        mesh.name = "Procedural Grid";
+
+        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
+        Vector2[] uv = new Vector2[vertices.Length];
+        for (int i = 0, z = 0; z <= zSize; z++) {
+            for (int x = 0; x <= xSize; x++, i++) {
+                float y = Mathf.PerlinNoise(x * .3f, z * .3f) * 2f;
+                vertices[i] = new Vector3(x, y, z);
+                uv[i] = new Vector2((float)x / xSize, (float)z / zSize);
+            }
+        }
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+
+        int[] triangles = new int[xSize * zSize * 6];
+        for (int t = 0, v = 0, z = 0; z < zSize; z++, v++) {
+            for (int x = 0; x < xSize; x++, t += 6, v++) {
+                triangles[t] = v;
+                triangles[t + 3] = triangles[t + 2] = v + 1;
+                triangles[t + 4] = triangles[t + 1] = v + xSize + 1;
+                triangles[t + 5] = v + xSize + 2;
+            }
+        }
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+    }
+
+    private void Awake() {
+        Generate();
+    }
+
+    private void OnDrawGizmos() {
+        if (vertices == null) {
+            return;
+        }
+        Gizmos.color = Color.black;
+        for (int i = 0; i < vertices.Length; i++) {
+            Gizmos.DrawSphere(vertices[i], 0.1f);
+        }
+    }
+
+}
